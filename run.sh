5956b9912e6722535eb9ac13b6f71435b3e0ba98
Bruce_zhang <zjh199601283492@gmail.com>
2018-03-31 13:13:03 -0400

    first

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-g++ -std=c++11 main.cpp atom.cpp -o out
-./out
+declare -a temp=(5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80)
+j=5;
+for i in ${temp[@]}
+do
+    echo $i
+    sed -i -e "s/double t=$j/""double t=$i/g" main.cpp
+    g++ -std=c++11 main.cpp atom.cpp -o out
+    ./out >>ener.txt
+    j=$i;
+done
